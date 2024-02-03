65108f862b088b935ffe571ea1626459e4925589
Anastassia <Anastassia.ana.turkina@gmail.com>
2024-02-03 16:19:00 +0200

    Update API call to use imported polygonKey
    API call in apiCalls.js now uses the imported polygonKey from keys.js file instead of a hardcoded key. This change helps improve the security of the application by preventing the API key from being exposed in the code.

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,10 +1,12 @@
+import {polygonKey} from "../../keys";
+
 export async function getStockInformationClosePrice(stock, setClosePrice){
     try{
         const requestOptions = {
             method: 'GET',
             redirect: 'follow',
         };
-        const response = await (await fetch(`https://api.polygon.io/v2/aggs/ticker/${stock}/range/1/day/2023-01-09/2023-01-09?apiKey=wU96Rp6clvICXRNdniivAJhr3vZNVGQn`, requestOptions)).json()
+        const response = await (await fetch(`https://api.polygon.io/v2/aggs/ticker/${stock}/range/1/day/2023-01-09/2023-01-09?apiKey=${polygonKey}`, requestOptions)).json()
         await console.log("response", response.results[0].c)
         setClosePrice(response.results[0].c)
     }
